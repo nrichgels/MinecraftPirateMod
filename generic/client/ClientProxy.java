8ebedde6db58c4b70509ac34eed23bec96d13050
nrichgels <richgelsna@mnstate.edu>
2015-03-02 15:51:46 -0600

    Added class ClientProxy to ClientProxy.java

---
+++ generic/client/ClientProxy.java
@@ -0,0 +1,13 @@
+package nathan.generic.client;
+
+import net.minecraftforge.client.MinecraftForgeClient;
+import nathan.generic.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+        
+        @Override
+        public void registerRenderers() {
+                // This is for rendering entities and so forth later on
+        }
+        
+}// End class ClientProxy
