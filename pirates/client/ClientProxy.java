a80b8c734526eafebb298e167b1b4c0433d8819a
nrichgels <richgelsna@mnstate.edu>
2015-03-02 19:10:44 -0600

    Added client proxy to pirates mod.  MC no longer crashes when debugged.

---
+++ pirates/client/ClientProxy.java
@@ -0,0 +1,13 @@
+package nathan.pirates.client;
+
+import net.minecraftforge.client.MinecraftForgeClient;
+import nathan.pirates.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+        
+        @Override
+        public void registerRenderers() {
+                // This is for rendering entities and so forth later on
+        }
+        
+}// End class ClientProxy
